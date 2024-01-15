0b09c2fd4ea23f4908534c0e1bd16093b8f6c48e
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-15 14:09:25 +0100

    feat(App): use store to display pastries

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,29 @@
 import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { addPastries } from './store/store';
 
 function App() {
+  const { pastries } = useSelector((state) => state.pastries);
+  const dispatch = useDispatch();
+
+  pastries.forEach((pastry) => {
+    console.log(pastry);
+  });
   return (
     <>
       <h1>Hello pastries!</h1>
+      <button
+        onClick={() => dispatch(addPastries({ name: 'Pain au chocolat' }))}
+      >
+        Click
+      </button>
+      {pastries && pastries.length > 0 && (
+        <ul>
+          {pastries.map((pastry, i) => (
+            <li key={i}>{pastry.name}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
