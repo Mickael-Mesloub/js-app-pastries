f96e1690e011a2a8311368d25e3ddf8244051f95
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-16 17:52:07 +0100

    styles: add styles to LoginPage

--- app/src/pages/LoginPage.jsx
+++ app/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../components/Layout';
 import { useEffect, useState } from 'react';
 import { login, logout } from '../store/auth';
+import './styles/LoginPage-styles.scss';
 
 const LoginPage = () => {
   const [user, setUser] = useState({
@@ -28,21 +29,24 @@ const LoginPage = () => {
   }, [state]);
 
   return (
-    <Layout>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-        <input type="submit" value="Login" />
-      </form>
-      <button onClick={handleLogout}>Logout</button>
+    <Layout className="login">
+      <>
+        <form onSubmit={handleSubmit} className="form-login">
+          <h2>Connectez-vous!</h2>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+          <input type="submit" value="Connexion" />
+        </form>
+        <button onClick={handleLogout}>Logout</button>
+      </>
     </Layout>
   );
 };
