d360d246dfddd5e8af0d97065980994d476043d3
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-16 11:18:59 +0100

    feat(LoginPage): implement basic form to test login and logout functions from the store

--- app/src/pages/LoginPage.jsx
+++ app/src/pages/LoginPage.jsx
@@ -1,9 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../components/Layout';
+import { useEffect } from 'react';
+import { login, logout } from '../store/user';
 
 const LoginPage = () => {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('>>>>Login');
+    dispatch(
+      login({
+        email: 'super-email@gmail.com',
+        password: '123',
+        isConnected: true,
+      })
+    );
+  };
+
+  const handleLogout = () => {
+    console.log('>>>>Logout');
+    dispatch(logout());
+  };
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <Layout>
-      <p>LoginPage</p>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <input type="submit" value="Login" />
+      </form>
+      <button onClick={handleLogout}>Logout</button>
     </Layout>
   );
 };
