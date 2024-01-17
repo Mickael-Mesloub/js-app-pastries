cc623e2d3042a5f1216f8dd9598db8776cb7fe1d
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-17 17:27:09 +0100

    feat(AddPastryForm): add styles and state to display/hide form

--- app/src/components/CrudForms/AddPastryForm.jsx
+++ app/src/components/CrudForms/AddPastryForm.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
+import CustomButton from '../CustomButton';
 import Layout from '../Layout';
+import './Forms-styles.scss';
 
 const AddPastryForm = () => {
+  const [formVisible, setFormVisible] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('AddPastryForm');
@@ -8,20 +13,39 @@ const AddPastryForm = () => {
   return (
     <Layout className="add-pastry">
       <>
-        <form onSubmit={handleSubmit} className="form-add-pastry">
-          <h2>Ajouter une pâtisserie</h2>
-          <input
-            type="text"
-            placeholder="Nom de la pâtisserie"
-            onChange={(e) => console.log(e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Quantité"
-            onChange={(e) => console.log(e.target.value)}
-          />
-          <input type="submit" value="Ajouter" />
-        </form>
+        <CustomButton
+          text={formVisible ? 'Fermer le formulaire' : 'Ajouter une pâtisserie'}
+          type={formVisible ? 'error' : 'success'}
+          onClick={() => setFormVisible(!formVisible)}
+        />
+        {formVisible && (
+          <form onSubmit={handleSubmit} className="form-add-pastry">
+            <h2>Ajouter une pâtisserie</h2>
+            <input
+              type="text"
+              placeholder="Nom de la pâtisserie"
+              onChange={(e) => console.log(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Quantité"
+              onChange={(e) => console.log(e.target.value)}
+            />
+            <div className="file-input-wrapper">
+              <input
+                type="file"
+                id="image"
+                name="image"
+                accept="image/jpeg, image/png"
+                onChange={(e) => console.log(e.target.files[0].name)}
+              />
+              <label htmlFor="image" className="file-input-label">
+                Choisissez une image
+              </label>
+            </div>
+            <input type="submit" value="Ajouter" />
+          </form>
+        )}
       </>
     </Layout>
   );
