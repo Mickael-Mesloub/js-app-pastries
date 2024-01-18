e07cddb40b16fbb317a556ae1a47a516093ce273
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-18 17:24:24 +0100

    refacto(PastriesList): remove unused imports

--- app/src/components/PastriesList.jsx
+++ app/src/components/PastriesList.jsx
@@ -1,12 +1,8 @@
 import './styles/PastriesList-styles.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect } from 'react';
 import { fetchAllPastriesAsync } from '../store/pastries';
 import PastryCard from './PastryCard';
-import Image from '../assets/images/pastries.jpg';
-import CustomModal from './CustomModal';
-import UpdatePastryForm from './CrudForms/UpdatePastryForm';
-
 const PastriesList = ({ isAdmin }) => {
   const authState = useSelector((state) => state.user);
   const { pastries } = useSelector((state) => state.pastries);
