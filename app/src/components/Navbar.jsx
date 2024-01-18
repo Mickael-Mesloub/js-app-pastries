4971f708bc477886b493bcf53121494b8292448b
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-01-18 10:23:57 +0100

    styles: edit styles and add logo

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import CustomButton from './CustomButton';
 import { Link } from 'react-router-dom';
 import { logout } from '../store/auth';
+import Logo from '../assets/images/logo-transparent.png';
 import './styles/Navbar-styles.scss';
 
 const Navbar = () => {
@@ -16,18 +17,29 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link">
-        Home
-      </Link>
-      <Link to="/contact" className="nav-link">
-        Contact
-      </Link>
-      <Link to="/login" className="nav-link">
-        {state.user.isConnected ? 'Admin' : 'Login'}
-      </Link>
-      {state.user.isConnected && (
-        <CustomButton text="Logout" type="logout" onClick={handleLogout} />
-      )}
+      <div className="logo">
+        <img
+          src={Logo}
+          width={250}
+          height={250}
+          aria-label="Company logo"
+          alt="Company logo"
+        />
+      </div>
+      <div className="links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+        <Link to="/login" className="nav-link">
+          {state.user.isConnected ? 'Admin' : 'Login'}
+        </Link>
+        {state.user.isConnected && (
+          <CustomButton text="Logout" type="logout" onClick={handleLogout} />
+        )}
+      </div>
     </nav>
   );
 };
